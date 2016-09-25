3220c4a3a18a6d132ba95a43e40088687e219b1f
Jakov Sosic <jsosic@gmail.com>
2016-09-26 01:39:42 +0200

    Add bunch of new params

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,30 +3,48 @@
 # This modules installs tinyproxy
 #
 class tinyproxy (
-  $port                = '8888',
-  $listen              = '',
-  $bind                = '',
-  $maxclients          = '100',
-  $minspareservers     = '5',
-  $maxspareservers     = '20',
-  $startservers        = '10',
-  $maxrequestsperchild = '0',
-  $allow               = [ '127.0.0.1' ],
-  $connectport         = [ '443', '563' ],
+  $port                 = '8888',
+  $listen               = '',
+  $bind                 = '',
+  $timeout              = '600',
+  $maxclients           = '100',
+  $minspareservers      = '5',
+  $maxspareservers      = '20',
+  $startservers         = '10',
+  $maxrequestsperchild  = '0',
+  $allow                = [ '127.0.0.1' ],
+  $viaproxyname         = 'tinyproxy',
+  $filter               = undef,
+  $filter_source        = undef,
+  $filter_urls          = false,
+  $filter_extended      = false,
+  $filter_casesensitive = false,
+  $filter_default_deny  = false,
+  $connectport          = [ '443', '563' ],
 ) {
+
+  File {
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    require => Package['tinyproxy'],
+    notify  => Service['tinyproxy'],
+  }
 
   package { 'tinyproxy':
     ensure  => present,
   }
 
   file { '/etc/tinyproxy/tinyproxy.conf':
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
     content => template('tinyproxy/tinyproxy.conf.erb'),
-    require => Package['tinyproxy'],
-    notify  => Service['tinyproxy'],
+  }
+
+  if ( $filter != undef ) {
+    file { '/etc/tinyproxy/filter':
+      path   => $filter,
+      source => $filter_source,
+    }
   }
 
   service { 'tinyproxy':
